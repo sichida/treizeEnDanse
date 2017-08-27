ed7145aaa36608d971909c81ba07722fac7ee630
Shoun Ichida <shoun@ichida.fr>
2017-08-27 13:28:14 +0200

    Added 2 pictures + updated menu label

--- src/app/services/media.service.ts
+++ src/app/services/media.service.ts
@@ -11,7 +11,9 @@ export class MediaService {
         {title: '', link: 'assets/images/gallery/5-gallery.png', thumb: 'assets/images/gallery/thumb/5-gallery.png'},
         {title: '', link: 'assets/images/gallery/6-gallery.jpg', thumb: 'assets/images/gallery/thumb/6-gallery.jpg'},
         {title: '', link: 'assets/images/gallery/7-gallery.jpg', thumb: 'assets/images/gallery/thumb/7-gallery.jpg'},
-        {title: '', link: 'assets/images/gallery/8-gallery.jpg', thumb: 'assets/images/gallery/thumb/8-gallery.jpg'}
+        {title: '', link: 'assets/images/gallery/8-gallery.jpg', thumb: 'assets/images/gallery/thumb/8-gallery.jpg'},
+        {title: '', link: 'assets/images/gallery/9-gallery.png', thumb: 'assets/images/gallery/thumb/9-gallery.png'},
+        {title: '', link: 'assets/images/gallery/10-gallery.png', thumb: 'assets/images/gallery/thumb/10-gallery.png'}
     ];
 
     constructor() {
