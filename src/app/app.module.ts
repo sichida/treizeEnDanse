e084830193063da8eef5e701bc2a727f3f2ffe87
Shoun Ichida <shoun@ichida.fr>
2018-07-19 23:37:24 +0200

    Content update (#5)
    * Content updated according to Clothilde inputs
    
    * Updated pictures
    
    * Added 2 pictures + updated menu label
    
    * Content is ready for production
    
    * Adding Google Analytics
    
    * Update content for 2018/2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,12 @@ import { MenuCmpComponent } from './menu-cmp/menu-cmp.component';
 import { HomeCmpComponent } from './home-cmp/home-cmp.component';
 import { MediaCmpComponent } from './media-cmp/media-cmp.component';
 import { PlanningCmpComponent } from './planning-cmp/planning-cmp.component';
-import { LinksCmpComponent } from './links-cmp/links-cmp.component';
 import { AccessCmpComponent } from './access-cmp/access-cmp.component';
 import { ProfileCmpComponent } from './profile-cmp/profile-cmp.component';
 import { VieEcoleCmpComponent } from './vie-ecole-cmp/vie-ecole-cmp.component';
 import { FacebookCmpComponent } from './facebook-cmp/facebook-cmp.component';
 import { MediaListComponent } from './media-list/media-list.component';
+import { InfosComponent } from './infos/infos.component';
 
 @NgModule({
   declarations: [
@@ -29,12 +29,12 @@ import { MediaListComponent } from './media-list/media-list.component';
     HomeCmpComponent,
     MediaCmpComponent,
     PlanningCmpComponent,
-    LinksCmpComponent,
     AccessCmpComponent,
     ProfileCmpComponent,
     VieEcoleCmpComponent,
     FacebookCmpComponent,
-    MediaListComponent
+    MediaListComponent,
+    InfosComponent
   ],
   imports: [
     BrowserModule,
@@ -45,4 +45,5 @@ import { MediaListComponent } from './media-list/media-list.component';
   providers: [FacebookService, MediaService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
